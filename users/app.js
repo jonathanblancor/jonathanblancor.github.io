eceef81a75e81de24591de0426723702952c52d9
Jonathan Blanco <jblan053@fiu.edu>
2018-05-11 18:31:35 -0400

    user test

---
+++ users/app.js
@@ -0,0 +1,21 @@
+angular.module('usersApp', [])
+
+.controller('mainController', function($scope, $http) {
+
+  $scope.sortType = 'first_name';
+  $scope.sortReverse = false;
+  $scope.searchUser = '';
+	
+	$http.get("user.json", function(response){
+		 $scope.users = response.data;
+	}, function(error) {
+		alert("Error loading file: " + error.data);
+	});
+  	
+	$scope.delete = function(index) {
+		if (index != -1) {
+			$scope.users.splice(index, 1);
+		}
+	}
+	
+});
