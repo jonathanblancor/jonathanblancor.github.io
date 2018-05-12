7a3ab2b706d35b23a15275d425a265854118ee1c
Jonathan Blanco <jblan053@fiu.edu>
2018-05-12 13:41:14 -0400

    update

--- users/app.js
+++ users/app.js
@@ -8,14 +8,47 @@ angular.module('usersApp', [])
 	
 	$http.get("data/users.json").then(function(response){
 		 $scope.users = response.data;
+		 $scope.usersFiltered = response.data;
 	}, function(error) {
 		alert("Error loading file: " + error.data);
 	});
-  	
-	$scope.delete = function(user) {
-		var index = $scope.users.findIndex(function(obj) {return obj.id == user.id});
+	  	
+	$scope.delete = function() {
 		
-		$scope.users.splice(index, 1);
+		var tempArray = angular.copy($scope.usersFiltered);
+		var index = 0;
+		
+		tempArray.forEach(function(obj) {
+				
+			if (obj.delete) {
+				index = $scope.users.findIndex(function(user) {return obj.id == user.id});
+				
+				if (index != -1)
+					$scope.users.splice(index, 1);
+				
+				index = $scope.usersFiltered.findIndex(function(user) {return obj.id == user.id});
+
+				if (index != -1)
+					$scope.usersFiltered.splice(index, 1);
+			}
+		});
+	}
+	
+	$scope.filter = function(filter){
+		if (filter == 'All') {
+			$scope.usersFiltered = $scope.users; 
+		}
+		else {
+			$scope.usersFiltered = [];
+			
+			$scope.users.forEach(function(obj) {
+				
+				if (obj.gender == filter) {
+					$scope.usersFiltered.push(obj);
+				}
+			});
+		}
+		
 	}
 	
 });
